7654da92da2660f83761be1391d6aa61ea61d85a
ido.moshe.fogel@gmail.com <ido.moshe.fogel@gmail.com>
2025-01-07 18:49:30 +0200

    coments in collection

--- app/Models/Collection.php
+++ app/Models/Collection.php
@@ -16,8 +16,6 @@ class Collection extends Model
     ];
     public function products()
     {
-        // return $this->hasMany(Product::class, 'collection_shopify_id', 'shopify_id');
-        // return $this->hasMany(Product::class, 'collection_id');//last one 3/1/2024
         return $this->belongsToMany(Product::class, 'collection_product', 'collection_id', 'product_id');
     }
     public $timestamps = false;
